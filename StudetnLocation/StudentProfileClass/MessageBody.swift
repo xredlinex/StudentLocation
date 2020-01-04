455479b8f8e46e5841f355da3d9536a5d5bc5f41
xredlinex <redik.line@gmail.com>
2020-01-04 12:11:56 +0200

    bladadada

---
+++ StudetnLocation/StudentProfileClass/MessageBody.swift
@@ -0,0 +1,27 @@
+//
+//  MessageBody.swift
+//  StudetnLocation
+//
+//  Created by alexey sorochan on 04.01.2020.
+//  Copyright © 2020 alexey sorochan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class EmailMessageBodyText {
+    func emailMessageBodyText(_ latitude: Double,_ longitude: Double) -> String{
+        let message = """
+        <html>
+          <head>
+            <title>Simple Map</title>
+
+          </head>
+          <body>
+            <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=\(latitude)%2C%20\(longitude)&t=&z=18&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+          </body>
+        </html>
+        """
+        return message
+    }
+}
